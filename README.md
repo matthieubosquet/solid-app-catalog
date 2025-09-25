7018ac5afd3f7cfabb47e3d7434777450bb3dfd6
Samu Lang <langsamu@hotmail.com>
2025-09-25 15:29:02 +0100

    Add a minimal next.js & LDO app

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # solid-app-catalog
+
+Minimal next.js app with LDO.
+
+See [home page](src/app/page.tsx) for demo.
---
+++ src/app/page.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+import {
+  parseRdf,
+  startTransaction,
+  toSparqlUpdate,
+  toTurtle,
+  set,
+} from "@ldo/ldo";
+import { FoafProfileShapeType } from "../.ldo/foafProfile.shapeTypes";
+
+export default async function Home() {
+  const rawTurtle = `
+  <#me> a <http://xmlns.com/foaf/0.1/Person>;
+      <http://xmlns.com/foaf/0.1/name> "Jane Doe".
+  `;
+  const ldoDataset = await parseRdf(rawTurtle, {
+    baseIRI: "https://solidweb.me/jane_doe/profile/card",
+  });
+  const janeProfile = ldoDataset
+    // Tells the LDO dataset that we're looking for a FoafProfile
+    .usingType(FoafProfileShapeType)
+    // Says the subject of the FoafProfile
+    .fromSubject("https://solidweb.me/jane_doe/profile/card#me");
+
+  return (
+    <div>
+      {janeProfile.name}
+    </div>
+  );
+}
