b4371f607d94b0e3ea40bd94ebd3d154b99e3a8f
Samu Lang <langsamu@hotmail.com>
2025-10-10 15:36:51 +0100

    cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,45 @@
+import { parseRdf } from "@ldo/ldo"
+import { SolidAppsShapeType } from "@/ldo/Model.shapeTypes"
+
+const resourceUri = "http://localhost:3001/mutate/resource.ttl" // TODO: Extract
+
 export default async function Home() {
+  var catalogueResponse = await fetch(resourceUri)
+  var catalogueRdf = await catalogueResponse.text()
+  const catalogueDataset = await parseRdf(catalogueRdf)
+  const catalogue = catalogueDataset
+    .usingType(SolidAppsShapeType)
+    .fromSubject("urn:example:solid-apps")
+
   return (
-    <div>
-      <div><a href="authn">click here for a authn browser login demo</a></div>
-      <div><a href="solidFetchServerLdo">click here for a server fetch ldo demo (requires CSS running)</a></div>
-      <div><a href="mutate">server fetch ldo put (requires CSS running)</a></div>
-    </div>
-  );
+    <ul>
+      {catalogue.app?.map(app =>
+        <li key={app.website!["@id"]}>
+          <dl>
+            <div>
+              <dt>name</dt>
+              <dd>{app.name}</dd>
+            </div>
+            <div>
+              <dt>description</dt>
+              <dd>{app.description}</dd>
+            </div>
+            <div>
+              <dt>featured</dt>
+              <dd><input type="checkbox" checked={app.featured} disabled></input></dd>
+            </div>
+            <div>
+              <dt>website</dt>
+              <dd><a href={app.website!["@id"]}>{app.website!["@id"]}</a></dd>
+            </div>
+            <div>
+              <dt>thumbnail</dt>
+              {/* TODO: remove style */}
+              <dd><img src={app.thumbnail!["@id"]} style={{ maxWidth: 100, maxHeight: 100 }}></img></dd>
+            </div>
+          </dl>
+        </li>
+      )}
+    </ul>
+  )
 }
